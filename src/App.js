7836df8b521ee36d97618efe5686fde367adb4ba
Eloise Chilvers <eloisechilvers@Eloises-MBP.lan>
2022-03-07 12:32:54 +0000

    add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
+import { Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Nav />
+      <Routes>
+
+      </Routes>
     </div>
   );
 }

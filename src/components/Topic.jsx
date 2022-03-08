527fccb82ac47c9a7fe879f61c3cc01d25319d66
Eloise Chilvers <eloisechilvers@Eloises-MacBook-Pro.local>
2022-03-08 10:00:58 +0000

    add topic page with articles

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -1,6 +1,30 @@
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ArticleCard from "./ArticleCard";
+import { getArticlesByTopic } from "../utils/api";
+import { useParams } from "react-router-dom";
 
-export default function Topic () {
+export default function Topic() {
+  const [articlesByTopic, setArticlesByTopic] = useState([]);
+  const { topic_slug } = useParams();
+  console.log(topic_slug, "topic");
+  useEffect(
+    () => {
+      getArticlesByTopic(topic_slug).then((articlesByTopic) => {
+        setArticlesByTopic(articlesByTopic);
+      });
+    },
+    [topic_slug]
+  );
 
-    return <p>topic</p>
+  return (
+    <main>
+       <section className="ArticleList__container">
+      {articlesByTopic.map((article) => {
+        return (
+            <ArticleCard key={article.article_id} {...article}/>
+        );
+      })}
+    </section>
+    </main>
+  );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://nc-news-ec.herokuapp.com/api"
-})
+  baseURL: "https://nc-news-ec.herokuapp.com/api",
+});
 
-export function getTopics () {
-    return api.get("/topics").then((res) => {
-        return res.data.topics;
-    })
+export function getTopics() {
+  return api.get("/topics").then((res) => {
+    return res.data.topics;
+  });
 }
 
-// export function getArticlesByTopic (topic_slug) {
-//     return api.get(`articles?topic=${topic_slug}`)
-// }
+export function getArticlesByTopic(topic_slug) {
+  console.log(topic_slug, "in get function")
+  return api.get(`/articles?topic=${topic_slug}`).then((res) => {
+    return res.data.articles;
+  });
+}
 
-export function getArticles () {
-    return api.get("/articles").then((res) => {
-        return res.data.articles;
-    })
+export function getArticles() {
+  return api.get("/articles").then((res) => {
+    return res.data.articles;
+  });
 }

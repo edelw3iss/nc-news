41c69c86ff4fafd0477bd430fc9bc6a8e5376269
Eloise Chilvers <eloisechilvers@Eloises-MacBook-Pro.local>
2022-03-08 10:53:50 +0000

    refactor to add Loading

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -5,26 +5,31 @@ import { useParams } from "react-router-dom";
 
 export default function Topic() {
   const [articlesByTopic, setArticlesByTopic] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { topic_slug } = useParams();
   console.log(topic_slug, "topic");
-  useEffect(
-    () => {
-      getArticlesByTopic(topic_slug).then((articlesByTopic) => {
-        setArticlesByTopic(articlesByTopic);
-      });
-    },
-    [topic_slug]
-  );
+  useEffect(() => {
+    getArticlesByTopic(topic_slug).then((articlesByTopic) => {
+      setArticlesByTopic(articlesByTopic);
+      setIsLoading(false);
+    });
+  }, [topic_slug]);
 
-  return (
+  return isLoading ? (
+    <h2>Loading...</h2>
+  ) : (
     <main>
-       <section className="ArticleList__container">
-      {articlesByTopic.map((article) => {
-        return (
-            <ArticleCard key={article.article_id} {...article}/>
-        );
-      })}
-    </section>
+      <section className="ArticleList__container">
+        {articlesByTopic.map((article) => {
+          return (
+            <ArticleCard
+              classname={topic_slug}
+              key={article.article_id}
+              {...article}
+            />
+          );
+        })}
+      </section>
     </main>
   );
 }

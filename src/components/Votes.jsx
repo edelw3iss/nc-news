e848138a4fea0523880a23fe8834668175639e7d
Eloise Chilvers <eloisechilvers@Eloises-MBP.home>
2022-03-09 11:07:23 +0000

    refactor like buttons

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,23 +1,55 @@
 import { useState } from "react";
 import { patchVotes } from "../utils/api";
 
-export default function Votes({articleId, votes}) {
+export default function Votes({ articleId, votes }) {
   const [voteDisp, setVoteDisp] = useState(votes);
+  const [buttonDisabled, setButtonDisabled] = useState({
+    plus: false,
+    minus: true,
+  });
   const handleClick = (voteInc) => {
     setVoteDisp(voteDisp + voteInc);
+    setButtonDisabled((currentButtonDisabled) => {
+      const newButtonDisabled = { ...currentButtonDisabled };
+      if (voteInc === 1) {
+        newButtonDisabled.plus = true;
+        newButtonDisabled.minus = false;
+      }
+      if (voteInc === -1) {
+        newButtonDisabled.minus = true;
+        newButtonDisabled.plus = false;
+      }
+      return newButtonDisabled;
+    });
     patchVotes(articleId, voteInc).catch((err) => {
       console.log(err);
-    })
-  }
-  
-  
+    });
+  };
+
   return (
-    <section >
-      <h4>Votes {voteDisp}</h4>
-      <button onClick={() => {handleClick(1)}}>👍</button>
-      <button onClick={() => {handleClick(-1)}} className=".LikeButtons__button">👎</button>
+    <section>
+      <h4>Likes {voteDisp}</h4>
+      
+      <button
+        className={`button--disabled_${buttonDisabled.plus}`}
+        
+        onClick={() => {
+          handleClick(1);
+        }}
+      >
+        👍 Like
+      </button>
+      <p className={`button--disabled_${buttonDisabled.minus}`}>👍 Liked!</p>
+      <button
+      className={`button--disabled_${buttonDisabled.minus}`}
+        
+        onClick={() => {
+          handleClick(-1);
+        }}
+        
+      >
+        Unlike
+      </button>
     </section>
   );
 }
-
-

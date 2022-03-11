3322b0647a4f8ea6ef3f6e8e0a7b93c689d3be2c
Eloise Chilvers <eloisechilvers@Eloises-MBP.home>
2022-03-11 14:33:51 +0000

    refactor to work on error handling

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -34,7 +34,7 @@ export default function Comments({ articleId }) {
               <p>
                 <i className="fa-solid fa-thumbs-up"></i> {comment.votes}
               </p>
-              <DeleteComment comment_id={comment.comment_id} author={comment.author} comments={comments} setComments={setComments}/>
+              <DeleteComment comment={comment} comments={comments} setComments={setComments}/>
             </article>
           );
         })}
@@ -42,3 +42,6 @@ export default function Comments({ articleId }) {
     </section>
   );
 }
+
+
+// comment_id={comment.comment_id} author={comment.author} 
--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,18 +1,22 @@
 import { UserContext } from "../contexts/UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { deleteComment } from "../utils/api";
 
-export default function ({ comment_id, author, comments, setComments }) {
+export default function ({ comment, comments, setComments }) {
+  const {comment_id, author} = comment;
   const { loggedInUser } = useContext(UserContext);
-  console.log(comment_id, author, "in delete comment");
-
+  const [isError, setIsError] = useState(false)
+    
+    console.log(comment_id, author, "in delete comment");
   const handleClick = () => {
     console.log(comment_id, "in delete button");
+    setIsError(false);
     setComments((currComments) =>
       currComments.filter((comment) => comment.comment_id !== comment_id)
     );
     deleteComment(comment_id).catch((err) => {
-      console.log(err);
+      setIsError(true);
+    //   setComments((currentComments) => currentComments.push(comment))
     });
   };
 

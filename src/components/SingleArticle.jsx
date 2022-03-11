0ca938ef4fccec2d8650dd3248828b60194b6c4b
Eloise Chilvers <eloisechilvers@Eloises-MBP.home>
2022-03-11 14:55:38 +0000

    error page

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -4,29 +4,50 @@ import { getArticleById } from "../utils/api";
 import Votes from "./Votes";
 import formatDate from "../utils/formatDate";
 import Comments from "./CommentList";
+import ErrorPage from "./ErrorPage";
 
 export default function SingleArticle() {
   const [article, setArticle] = useState({});
   const { article_id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    getArticleById(article_id).then((articleData) => {
-      setArticle(articleData);
-      setIsLoading(false);
-    });
+    getArticleById(article_id)
+      .then((articleData) => {
+        setArticle(articleData);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch(
+        ({
+          response: {
+            data: { msg },
+            status,
+          },
+        }) => {
+          console.log(msg, status, "ERROR");
+          setError({ msg, status });
+          setIsLoading(false);
+        }
+      );
   }, [article_id]);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (error) return <ErrorPage msg={error.msg}/>
+
+  return (
+  <main className="SingleArticle__main">
+    <h2>{article.title}</h2>
+    <h3>{article.author}</h3>
+    <h4>{formatDate(article.created_at)}</h4>
+    <p className="SingleArticle__topic">{article.topic}</p>
+    <p className="SingleArticle__body">{article.body}</p>
+    <Votes articleId={article.article_id} votes={article.votes} />
+    <Comments articleId={article.article_id} />
+  </main>
+  )
   
-  return isLoading ? (
-    <h2>Loading...</h2>
-  ) : (
-    <main className="SingleArticle__main">
-      <h2>{article.title}</h2>
-      <h3>{article.author}</h3>
-      <h4>{formatDate(article.created_at)}</h4>
-      <p className="SingleArticle__topic">{article.topic}</p>
-      <p className="SingleArticle__body">{article.body}</p>
-      <Votes articleId={article.article_id} votes={article.votes} />
-      <Comments articleId={article.article_id} />
-    </main>
-  );
 }
